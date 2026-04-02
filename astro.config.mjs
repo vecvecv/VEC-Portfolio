bc3cd74b1a1ddffd7dc2626dfb786750f71fe5b8
vecvecv <valdiviezocristinae@gmail.com>
2026-04-02 18:29:56 -0300

    fix internal links for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-        site: 'https://vecvecv.github.io',
-        base: '/VEC-Portfolio',
-
+    site: 'https://vecvecv.github.io',
+    base: '/VEC-Portfolio',
+    output: 'static',
 });
